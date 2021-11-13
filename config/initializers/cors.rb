153612d814c5ad4cb4377db6d07b7f553f081fa1
mariordgez <mariordgez@hotmail.com>
2021-11-13 05:52:24 -0700

    Changed cors file to allow cross origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,15 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '/v1/*',
+      headers: :'accept',
+      methods: [:get]
+  end
+end
 
 # Rails.application.config.middleware.insert_before 0, Rack::Cors do
 #   allow do
